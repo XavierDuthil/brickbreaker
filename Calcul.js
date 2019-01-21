7c49d977c20409e2ec1219accbc46628274a92fe
Xavier Duthil <xduthil@qosenergy.com>
2019-01-21 16:18:02 +0100

    Add scoring and increasing speed

--- Calcul.js
+++ Calcul.js
@@ -4,26 +4,27 @@ var myInterval;
 
 document.getElementById("reset").onclick = function()
 {
+	var dateDebutJeu = new Date();
 	clearInterval(myInterval);
-	
+
 	// Création du canvas
 	var canvas = new CL_Canvas();
 	canvas.constr();
-	
+
 	// Création de la balle
 	var balle = new CL_Balle();
 	balle.constr(20, 205, canvas.getHeight() - 120, 5, Math.floor(Math.random() * 90) + 135); // diametre, posX, posY, vitesse, direction
-	
+
 	// Création de la raquette
 	var raquette = new CL_Raquette();
 	raquette.constr(100, 20, 225, 100); // largeurRaquette, hauteurRaquette, posX, posY
 
 	// Création du compteur de vies, initialisation à 3 vies
 	var vie = new compteurVies();
-	vie.setVies(3);
-	
-    myInterval = setInterval(animate, 1000/60);
-	
+	vie.setVies(1);
+
+	myInterval = setInterval(animate, 1000/60);
+
 	function animate()
 	{
 		// Correspond aux coordonnées supposées à l'image suivante
@@ -43,27 +44,28 @@ document.getElementById("reset").onclick = function()
 			balle.setPosX(Math.round(balle.getPosX() + (balle.getVitesse() * Math.sin(balle.getDirection() * Math.PI / 180))))
 			balle.setPosY(Math.round(balle.getPosY() + (balle.getVitesse() * Math.cos(balle.getDirection() * Math.PI / 180))));
 		}
-		
-		// Acceleration de la vitesse verticale selon la pesanteur
-		//vitesseY -= 0.1;
-		
+
+		// Augmentation de la vitesse de la balle
+    balle.setVitesse(balle.getVitesse() + 0.01);
+
 		// Position de la raquette
 		if (souris.getMouseX() > 50 && souris.getMouseX() < canvas.getWidth() - 50)
 			raquette.setPosX(souris.getMouseX());
-			
+
 		else if (souris.getMouseX() < 50)
 			raquette.setPosX(50);
-			
+
 		else
 			raquette.setPosX(canvas.getWidth() - 50);
-		
+
+		document.getElementById("temps").textContent = (new Date() - dateDebutJeu) / 1000.0 + " s";
 		document.getElementById("direction").textContent = balle.getDirection();
 		document.getElementById("vitesse").textContent = balle.getVitesse();
 		document.getElementById("mouseX").textContent = souris.getMouseX();
 		document.getElementById("mouseY").textContent = souris.getMouseX();
 		document.getElementById("posX").textContent = balle.getPosX();
 		document.getElementById("posY").textContent = balle.getPosY();
-		
+
 		if (vie.vivant())
 		{
 			var stringVies = "";
@@ -71,19 +73,16 @@ document.getElementById("reset").onclick = function()
 			{
 				stringVies += " &#9829;";
 			}
-			
+
 			document.getElementById("echec").innerHTML = stringVies;
 		}
-		
+
 		else
 		{
 			document.getElementById("echec").innerHTML = "YOU LOSE";
 			clearInterval(myInterval);
 		}
-		
-		 //balle.setDirection(balle.getDirection() + 1);
-		 //balle.setVitesse(balle.getVitesse() + 1);
-		
+
 		// Redessine le canvas selon les coordonnées de chaque élément
 		canvas.draw(balle.getPosX(), balle.getPosY(), balle.getDiametre(), raquette.getPosX(), raquette.getPosY()); // ballePosX, ballePosY, diametreBalle, raquettePosX, raquettePosY
 	}
@@ -92,25 +91,25 @@ document.getElementById("reset").onclick = function()
 function compteurVies()
 {
 	var vies;
-	
+
 	this.setVies = function(nombre)
 	{
 		this.vies = nombre;
 	}
-	
+
 	this.mourir = function()
 	{
 		this.vies -= 1;
 	}
-	
+
 	this.vivant = function()
 	{
 		if (this.vies > 0)
 			return true;
-			
+
 		return false;
 	}
-	
+
 	this.getVies = function()
 	{
 		return this.vies;
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # brickbreaker
-One of my first personnal projects. The idea was to implement collisions in a 2D environment.
+One of my first personnal projects: a Brick Breaker without bricks. The idea was to implement collisions in a 2D environment.
+
+The goal is to keep the ball within the canvas as long as possible.
+
+The current "engine" has limitations concerning the collision calculation with the paddle when the speed gets too high (it passes through).
 
 # Screenshots
-[![screenshot](https://github.com/XavierDuthil/brickbreaker/raw/master/img/screenshot.jpg)]
+[![screenshot](https://github.com/XavierDuthil/brickbreaker/raw/master/img/screenshot.png)]
